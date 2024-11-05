afd71635e0192c5a629216be571b87a0b66f6635
Samer Bagla <sbagla369@gmail.com>
2024-11-05 12:38:16 +0000

    initail commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,27 @@
 
 // Vanilla JavaScript Function
 
+    function addTwoNumbers(a, b) {
+        // Code block
+        return a + b;
+    }
+
+    let sum =  addTwoNumbers(3, 5);
+    console.log(sum);
+
 // Arrow Function With Parameters
 
-// Single Line Arrow Function With Parameters
+    const addTwoNumbers = (a, b) => {
+    // Code block
+        return a + b;
+    }
 
+    let sum =  addTwoNumbers(3, 5);
+    console.log(sum);
+
+
+// Single Line Arrow Function With Parameters
+    const addTwoNumbers2 = (a, b) => a + b;
 // Implicit Returns
 
 // Returning Multiple Lines
